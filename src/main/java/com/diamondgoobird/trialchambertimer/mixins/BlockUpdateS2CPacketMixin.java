539612f04e1837872688973c39cba6cca7f6f23d
Diamondgoobird <birdsplaymc2@gmail.com>
2024-07-24 19:35:47 -0700

    Added support for multiplayer.
    Making a mixing that hooks into block updates, finding out when cooldowns start became easy.

---
+++ src/main/java/com/diamondgoobird/trialchambertimer/mixins/BlockUpdateS2CPacketMixin.java
@@ -0,0 +1,35 @@
+package com.diamondgoobird.trialchambertimer.mixins;
+
+import com.diamondgoobird.trialchambertimer.TrialChamberTimer;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.TrialSpawnerBlock;
+import net.minecraft.block.entity.TrialSpawnerBlockEntity;
+import net.minecraft.block.enums.TrialSpawnerState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.listener.ClientPlayPacketListener;
+import net.minecraft.network.packet.s2c.play.BlockUpdateS2CPacket;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BlockUpdateS2CPacket.class)
+public class BlockUpdateS2CPacketMixin {
+    @Shadow @Final private BlockState state;
+
+    @Shadow @Final private BlockPos pos;
+
+    @Inject(method = "apply(Lnet/minecraft/network/listener/ClientPlayPacketListener;)V", at = @At("HEAD"))
+    public void onApply(ClientPlayPacketListener clientPlayPacketListener, CallbackInfo ci) {
+        if (state.getBlock() instanceof TrialSpawnerBlock && state.getEntries().get(Properties.TRIAL_SPAWNER_STATE) == TrialSpawnerState.COOLDOWN) {
+            if (TrialChamberTimer.getTime(pos) == 0) {
+                assert MinecraftClient.getInstance().world != null;
+                TrialChamberTimer.insertTime(pos, MinecraftClient.getInstance().world.getTime() + ((TrialSpawnerBlockEntity) MinecraftClient.getInstance().world.getBlockEntity(pos)).getSpawner().cooldownLength);
+            }
+        }
+    }
+}
