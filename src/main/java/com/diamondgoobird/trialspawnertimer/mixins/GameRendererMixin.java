f18aaf71026631e55c37155ed0ba94e99c2dde37
Diamondgoobird <diamondgoobird@gmail.com>
2025-06-30 11:14:41 -0700

    Updated to 1.21.7 and simplified GameRendererMixin

--- src/main/java/com/diamondgoobird/trialspawnertimer/mixins/GameRendererMixin.java
+++ src/main/java/com/diamondgoobird/trialspawnertimer/mixins/GameRendererMixin.java
@@ -17,7 +17,7 @@ public class GameRendererMixin {
      * @param instance the instance of the MinecraftClient
      * @return if the user just ran /trialspawnertimer then a new ConfigScreen instance, otherwise just the current screen
      */
-    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;currentScreen:Lnet/minecraft/client/gui/screen/Screen;", opcode = Opcodes.GETFIELD))
+    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;currentScreen:Lnet/minecraft/client/gui/screen/Screen;", opcode = Opcodes.GETFIELD, ordinal = 0))
     public Screen redirectGetScreen(MinecraftClient instance) {
         /*
          Uses convoluted logic to display our GUI when the user runs /trialspawnertimer
