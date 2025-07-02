2d68847baf71226723b1da6f595866c36f512473
Diamondgoobird <diamondgoobird@gmail.com>
2025-07-02 09:56:32 -0700

    Downgraded to 1.21.4

--- src/main/java/com/diamondgoobird/trialspawnertimer/mixins/TrialSpawnerBlockEntityRendererMixin.java
+++ src/main/java/com/diamondgoobird/trialspawnertimer/mixins/TrialSpawnerBlockEntityRendererMixin.java
@@ -20,8 +20,8 @@ import static com.diamondgoobird.trialspawnertimer.TrialSpawnerTimer.*;
 public class TrialSpawnerBlockEntityRendererMixin {
     @Shadow @Final private EntityRenderDispatcher entityRenderDispatcher;
 
-    @Inject(method = "render(Lnet/minecraft/block/entity/TrialSpawnerBlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/util/math/Vec3d;)V", at = @At("RETURN"))
-    public void onRender(TrialSpawnerBlockEntity trialSpawnerBlockEntity, float f, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, int j, Vec3d vec3d, CallbackInfo ci) {
+    @Inject(method = "render(Lnet/minecraft/block/entity/TrialSpawnerBlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V", at = @At("RETURN"))
+    public void onRender(TrialSpawnerBlockEntity trialSpawnerBlockEntity, float f, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, int j, CallbackInfo ci) {
         // If there is no timer rendered, check for updates
         boolean rend = TimerRenderer.drawTimer(trialSpawnerBlockEntity.getWorld(), trialSpawnerBlockEntity, matrixStack, vertexConsumerProvider, entityRenderDispatcher, i);
         // If higher sensitivity is on and the text didn't render then check if we want to add a timer
