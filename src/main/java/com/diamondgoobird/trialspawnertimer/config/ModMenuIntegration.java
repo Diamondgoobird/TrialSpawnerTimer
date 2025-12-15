df421bfd9f0a9dadffc7880942dcdcfdc0e9f709
Diamondgoobird <diamondgoobird@gmail.com>
2025-12-14 19:21:25 -0800

    Added back modmenu integration

--- src/main/java/com/diamondgoobird/trialspawnertimer/config/ModMenuIntegration.java
+++ src/main/java/com/diamondgoobird/trialspawnertimer/config/ModMenuIntegration.java
@@ -1,15 +1,15 @@
 package com.diamondgoobird.trialspawnertimer.config;
 
-// import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-// import com.terraformersmc.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import net.minecraft.client.gui.screen.Screen;
 
 /**
  * Entrypoint for ModMenu so that players can get to the config screen easier
  */
-public class ModMenuIntegration /*implements ModMenuApi*/ {
-    // @Override
-    // public ConfigScreenFactory<?> getModConfigScreenFactory() {
-    //     return (ConfigScreenFactory<Screen>) ConfigScreen::new;
-    // }
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return (ConfigScreenFactory<Screen>) ConfigScreen::new;
+    }
 }
