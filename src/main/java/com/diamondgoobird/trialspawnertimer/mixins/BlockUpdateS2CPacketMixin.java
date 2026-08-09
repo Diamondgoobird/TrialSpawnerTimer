e4f7ac58b3ec9df7f91cd10cb179e088532eec76
diamondgoobird <diamondgoobird@gmail.com>
2026-08-09 15:30:45 -0700

    Moved Level null checking, removed redundant assertion, bumped version for logs.

--- src/main/java/com/diamondgoobird/trialspawnertimer/mixins/BlockUpdateS2CPacketMixin.java
+++ src/main/java/com/diamondgoobird/trialspawnertimer/mixins/BlockUpdateS2CPacketMixin.java
@@ -26,7 +26,10 @@ public abstract class BlockUpdateS2CPacketMixin {
         // We use the client's world because the packet doesn't send us one,
         // and we would only get block updates for the world we're in
         World w = MinecraftClient.getInstance().world;
-        assert w != null;
+        // If the world doesn't exist then don't update timers
+        if (w == null) {
+            return;
+        }
         // Check if our Trial Spawner was just destroyed and turned into air
         if (getState().getBlock() instanceof AirBlock) {
             // If we have a timer at the given position, delete it
