7f8251bb115a22d57ad7b7c3fcc09127c5ed9ed6
diamondgoobird <diamondgoobird@gmail.com>
2026-08-09 15:57:36 -0700

    Moved Level null checking, removed redundant assertion, bumped version for logs.

--- src/main/java/com/diamondgoobird/trialspawnertimer/mixins/TrialSpawnerBlockEntityRendererMixin.java
+++ src/main/java/com/diamondgoobird/trialspawnertimer/mixins/TrialSpawnerBlockEntityRendererMixin.java
@@ -26,9 +26,12 @@ public abstract class TrialSpawnerBlockEntityRendererMixin {
     @Inject(method = "render(Lnet/minecraft/client/render/block/entity/state/BlockEntityRenderState;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/command/OrderedRenderCommandQueue;Lnet/minecraft/client/render/state/CameraRenderState;)V", at = @At("RETURN"))
     public void onRender(BlockEntityRenderState state, MatrixStack matrices, OrderedRenderCommandQueue queue, CameraRenderState cameraState, CallbackInfo ci) {
         World w = MinecraftClient.getInstance().world;
-
+        // If the world doesn't exist then don't render
+        if (w == null) {
+            return;
+        }
         // If there is no timer rendered, check for updates
-        boolean rend = TimerRenderer.drawTimer(w, state.pos, matrices, w.getLightLevel(state.pos), queue, entityRenderDispatcher.camera);
+        TimerRenderer.drawTimer(w, state.pos, matrices, w.getLightLevel(state.pos), queue, entityRenderDispatcher.camera);
         // If higher sensitivity is on then check for updates
         if (getConfig().isHighSensitivity()) {
             onSpawnerStateUpdate(w, state.pos, state.blockState.get(Properties.TRIAL_SPAWNER_STATE));
